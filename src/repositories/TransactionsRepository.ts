296b3a7fc120f5d3e4e9c15b9d0c356665966074
Jonathan <jonathanbarreira@gmail.com>
2020-06-28 13:56:11 -0300

    Finalizando desafio com testes passando.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,42 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    if (this.transactions.length === 0) {
+      return {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      };
+    }
+
+    const balance = this.transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.value;
+        }
+        if (transaction.type === 'outcome') {
+          acc.outcome += transaction.value;
+        }
+        return acc;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
